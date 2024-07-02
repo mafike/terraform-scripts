8e9f8b5c53a8ee517d29a6a5c8f392a993a9b661
mafike <mafgenes1@icloud.com>
2024-07-02 00:23:18 -0400

    modules

--- server/main.tf
+++ server/main.tf
@@ -1,3 +1,9 @@
+/*
+Name: IaC Buildout for Terraform Associate Exam
+Description: AWS Infrastructure Buildout
+Contributors: Bryan and Gabe
+*/
+
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
@@ -7,11 +13,6 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 data "aws_region" "current" {}
 
-locals {
-  team        = "api_mgmt_dev"
-  application = "api_server"
-  server_name = "ec2-${var.environment}-api-${var.variables_sub_az}"
-}
 #Define the VPC 
 resource "aws_vpc" "vpc" {
   cidr_block = var.vpc_cidr
@@ -20,8 +21,9 @@ resource "aws_vpc" "vpc" {
     Name        = var.vpc_name
     Environment = "demo_environment"
     Terraform   = "true"
-    Region      = data.aws_region.current.name
   }
+
+  enable_dns_hostnames = true
 }
 
 #Deploy the private subnets
@@ -121,19 +123,100 @@ resource "aws_nat_gateway" "nat_gateway" {
   }
 }
 
-resource "aws_subnet" "variables-subnet" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.variables_sub_cidr
-  availability_zone       = var.variables_sub_az
-  map_public_ip_on_launch = var.variables_sub_auto_ip
+
+resource "random_string" "random" {
+  length = 10
+}
+
+# Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"]
+}
+
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "ubuntu_server" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t2.micro"
+  subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups             = [aws_security_group.vpc-ping.id, aws_security_group.ingress-ssh.id, aws_security_group.vpc-web.id]
+  associate_public_ip_address = true
+  key_name                    = aws_key_pair.generated.key_name
+  connection {
+    user        = "ubuntu"
+    private_key = tls_private_key.generated.private_key_pem
+    host        = self.public_ip
+  }
+
+  # Leave the first part of the block unchanged and create our `local-exec` provisioner
+  provisioner "local-exec" {
+    command = "chmod 600 ${local_file.private_key_pem.filename}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo rm -rf /tmp",
+      "sudo git clone https://github.com/hashicorp/demo-terraform-101 /tmp",
+      "sudo sh /tmp/assets/setup-web.sh",
+    ]
+  }
 
   tags = {
-    Name      = "sub-variables-${var.variables_sub_az}"
-    Terraform = "true"
+    Name = "Ubuntu EC2 Server"
+  }
+
+  lifecycle {
+    ignore_changes = [security_groups]
+  }
+
+
+}
+
+# Terraform Resource Block - Security Group to Allow Ping Traffic
+resource "aws_security_group" "vpc-ping" {
+  name        = "vpc-ping"
+  vpc_id      = aws_vpc.vpc.id
+  description = "ICMP for Ping Access"
+  ingress {
+    description = "Allow ICMP Traffic"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    description = "Allow all ip and ports outboun"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
-# Security Groups
+resource "tls_private_key" "generated" {
+  algorithm = "RSA"
+}
+
+resource "local_file" "private_key_pem" {
+  content  = tls_private_key.generated.private_key_pem
+  filename = "MyAWSKey.pem"
+}
+
+resource "aws_key_pair" "generated" {
+  key_name   = "MyAWSKey"
+  public_key = tls_private_key.generated.public_key_openssh
+}
 
 resource "aws_security_group" "ingress-ssh" {
   name   = "allow-all-ssh"
@@ -155,7 +238,6 @@ resource "aws_security_group" "ingress-ssh" {
   }
 }
 
-# Create Security Group - Web Traffic
 resource "aws_security_group" "vpc-web" {
   name        = "vpc-web-${terraform.workspace}"
   vpc_id      = aws_vpc.vpc.id
@@ -185,43 +267,8 @@ resource "aws_security_group" "vpc-web" {
   }
 }
 
-resource "aws_security_group" "vpc-ping" {
-  name        = "vpc-ping"
-  vpc_id      = aws_vpc.vpc.id
-  description = "ICMP for Ping Access"
-  ingress {
-    description = "Allow ICMP Traffic"
-    from_port   = -1
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  egress {
-    description = "Allow all ip and ports outboun"
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Terraform Data Block - To Lookup Latest Ubuntu 20.04 AMI Image
-data "aws_ami" "ubuntu" {
-  most_recent = true
-
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  owners = ["099720109477"]
-}
-resource "aws_instance" "ubuntu_server" {
+# Terraform Resource Block - To Build Web Server in Public Subnet
+resource "aws_instance" "web_server" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t2.micro"
   subnet_id                   = aws_subnet.public_subnets["public_subnet_1"].id
@@ -233,6 +280,7 @@ resource "aws_instance" "ubuntu_server" {
     private_key = tls_private_key.generated.private_key_pem
     host        = self.public_ip
   }
+
   # Leave the first part of the block unchanged and create our `local-exec` provisioner
   provisioner "local-exec" {
     command = "chmod 600 ${local_file.private_key_pem.filename}"
@@ -247,9 +295,7 @@ resource "aws_instance" "ubuntu_server" {
   }
 
   tags = {
-    Name  = local.server_name
-    Owner = local.team
-    App   = local.application
+    Name = "Web EC2 Server"
   }
 
   lifecycle {
@@ -258,31 +304,29 @@ resource "aws_instance" "ubuntu_server" {
 
 }
 
-resource "tls_private_key" "generated" {
-  algorithm = "RSA"
-}
-
-resource "local_file" "private_key_pem" {
-  content  = tls_private_key.generated.private_key_pem
-  filename = "MyAWSKey.pem"
-}
-
-resource "aws_key_pair" "generated" {
-  key_name   = "MyAWSKey"
-  public_key = tls_private_key.generated.public_key_openssh
-
-  lifecycle {
-    ignore_changes = [key_name]
+# Terraform Resource Block - To Build EC2 instance in Public Subnet
+resource "aws_instance" "web_server_2" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnets["public_subnet_2"].id
+  tags = {
+    Name = "Web EC2 Server 2"
   }
 }
 
-module "server" {
-  source          = "../server"
-  ami             = data.aws_ami.ubuntu.id
-  subnet_id       = aws_subnet.public_subnets["public_subnet_3"].id
-  security_groups = [
-    aws_security_group.vpc-ping.id,
-    aws_security_group.ingress-ssh.id,
-    aws_security_group.vpc-web.id
-  ]
+output "public_ip" {
+  value = aws_instance.ubuntu_server.public_ip
 }
+
+output "public_dns" {
+  value = aws_instance.ubuntu_server.public_dns
+}
+
+output "public_ip_server_subnet_1" {
+  value = aws_instance.web_server.public_ip
+}
+
+output "public_dns_server_subnet_1" {
+  value = aws_instance.web_server.public_dns
+}
+
