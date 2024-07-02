9a1d614b329409e20466f9f16cb24dae1d7d2adb
mafike <mafgenes1@icloud.com>
2024-07-02 01:00:47 -0400

    modules

--- ex1/main.tf
+++ ex1/main.tf
@@ -277,7 +277,7 @@ resource "aws_key_pair" "generated" {
 }
 
 module "server" {
-  source          = "../server"
+  source          = "../modules/server"
   ami             = data.aws_ami.ubuntu.id
   subnet_id       = aws_subnet.public_subnets["public_subnet_3"].id
   security_groups = [
@@ -285,4 +285,43 @@ module "server" {
     aws_security_group.ingress-ssh.id,
     aws_security_group.vpc-web.id
   ]
+}
+
+module "server_subnet_1" {
+  source          = "../modules/web_server"
+  ami             = data.aws_ami.ubuntu.id
+  key_name        = aws_key_pair.generated.key_name
+  user            = "ubuntu"
+  private_key     = tls_private_key.generated.private_key_pem
+  subnet_id       = aws_subnet.public_subnets["public_subnet_1"].id
+  security_groups = [aws_security_group.vpc-ping.id, 
+  aws_security_group.ingress-ssh.id, 
+  aws_security_group.vpc-web.id]
+}
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "4.9.0"
+
+  # Autoscaling group
+  name = "myasg"
+
+  vpc_zone_identifier = [aws_subnet.private_subnets["private_subnet_1"].id, 
+  aws_subnet.private_subnets["private_subnet_2"].id, 
+  aws_subnet.private_subnets["private_subnet_3"].id]
+  min_size            = 0
+  max_size            = 1
+  desired_capacity    = 1
+
+  # Launch template
+  use_lt    = true
+  create_lt = true
+
+  image_id      = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+
+  tags_as_map = {
+    Name = "Web EC2 Server 2"
+  }
+
 }
