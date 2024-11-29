2f6033531fd28f4388c2deb145ccf582ef10cd25
mafike <mafgenes1@icloud.com>
2024-11-29 01:16:08 -0500

    changes in sc

--- eks/k8-ebs-sc/k8-sc.tf
+++ eks/k8-ebs-sc/k8-sc.tf
@@ -6,5 +6,5 @@ resource "kubernetes_storage_class_v1" "ebs_sc" {
   storage_provisioner = "ebs.csi.aws.com"
   volume_binding_mode = "WaitForFirstConsumer"
   allow_volume_expansion = "true"  
-  reclaim_policy = "Retain"
+ # reclaim_policy = "Retain"
 }
